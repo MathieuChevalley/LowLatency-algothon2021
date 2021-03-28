7215a1ae2b021229d68f31587a0e9c4b9ec1069d
Mathieu Chevalley <m.chevalley97@gmail.com>
2021-03-28 13:40:03 +0200

    change to cpp

--- Makefile
+++ Makefile
@@ -1,2 +1,8 @@
-run:
-	@python3 predict.py
+#run:
+#	@python3 predict.py
+bin/run: src/run.cpp
+	@mkdir -p bin
+	@g++ $^ -o bin/run
+
+run: bin/run
+	@bin/run
---
+++ src/run.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <iostream>
+
+int main() {
+	
+  std::string line;
+
+  while (std::cin >> line) {
+    std::cout << 1.0 << std::endl;
+  }
+
+  return 0;
+}
